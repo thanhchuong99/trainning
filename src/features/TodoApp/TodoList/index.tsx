357c27cc7f33a1e62fdedb714958fd89e441cd1c
thanhchuong99 <nguyenthanhchuonghth@gmail.com>
2022-02-28 11:27:31 +0700

    binding value form with ant-design

--- src/features/TodoApp/TodoList/index.tsx
+++ src/features/TodoApp/TodoList/index.tsx
@@ -1,4 +1,4 @@
-import { Col, Row, Input, Button, Select, Tag } from "antd";
+import { Col, Row, Input, Button, Select, Tag, Form } from "antd";
 import { ChangeEvent, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { v4 as uuidv4 } from "uuid";
@@ -12,34 +12,35 @@ export interface ITodo {
   priority: string;
   completed: boolean;
 }
+export interface TodoInput {
+  name: string;
+  priority: string;
+}
 export default function TodoList() {
-  const [inputText, setInputText] = useState("");
-  const [priority, setPriority] = useState("Medium");
+  // const [inputText, setInputText] = useState("");
+  // const [priority, setPriority] = useState("Medium");
 
   const todoList = useSelector(todoRemainingSelector);
   const dispatch = useDispatch();
 
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setInputText(e.target.value);
+  // const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+  //   setInputText(e.target.value);
+  // };
+  // const handleStatusChange = (value: string) => {
+  //   setPriority(value);
+  // };
+  const handleAddButtonClick = (input: TodoInput) => {
+    dispatch(
+      todoListSlice.actions.addTodo({
+        id: uuidv4(),
+        name: input.name,
+        priority: input.priority,
+        complete: false,
+      }),
+    );
+    form.resetFields();
   };
-  const handleStatusChange = (value: string) => {
-    setPriority(value);
-  };
-  const handleAddButtonClick = () => {
-    if (inputText.trim()) {
-      dispatch(
-        todoListSlice.actions.addTodo({
-          id: uuidv4(),
-          name: inputText,
-          priority: priority,
-          complete: false,
-        }),
-      );
-    }
-    setInputText("");
-    setPriority("Medium");
-  };
-
+  const [form] = Form.useForm();
   return (
     <Row style={{ height: "calc(100% - 40px)" }}>
       <Col span={24} style={{ height: "calc(100% - 40px)", overflowY: "auto" }}>
@@ -55,31 +56,47 @@ export default function TodoList() {
           ))}
       </Col>
       <Col span={24} style={{ marginTop: "50px" }}>
-        <Input.Group style={{ display: "flex" }} compact>
-          <Input value={inputText} onChange={handleInputChange} />
-          <Select
-            defaultValue="Medium"
-            value={priority}
-            onChange={handleStatusChange}
+        <Form
+          layout="inline"
+          onFinish={handleAddButtonClick}
+          initialValues={{
+            inputText: "",
+            priority: "Medium",
+          }}
+        >
+          <Form.Item
+            style={{ flex: 1 }}
+            name="name"
+            rules={[
+              { required: true, message: "Todo name is required" },
+              { whitespace: true },
+            ]}
           >
-            <Select.Option value="High" label="High">
-              <Tag color="red">High</Tag>
-            </Select.Option>
-            <Select.Option value="Medium" label="Medium">
-              <Tag color="blue">Medium</Tag>
-            </Select.Option>
-            <Select.Option value="Low" label="Low">
-              <Tag color="gray">Low</Tag>
-            </Select.Option>
-          </Select>
-          <Button
-            type="primary"
-            style={{ backgroundColor: " #1890ff" }}
-            onClick={handleAddButtonClick}
-          >
-            Add
-          </Button>
-        </Input.Group>
+            <Input />
+          </Form.Item>
+          <Form.Item name="priority">
+            <Select>
+              <Select.Option value="High" label="High">
+                <Tag color="red">High</Tag>
+              </Select.Option>
+              <Select.Option value="Medium" label="Medium">
+                <Tag color="blue">Medium</Tag>
+              </Select.Option>
+              <Select.Option value="Low" label="Low">
+                <Tag color="gray">Low</Tag>
+              </Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item style={{ marginRight: "0" }}>
+            <Button
+              type="primary"
+              style={{ backgroundColor: " #1890ff" }}
+              htmlType="submit"
+            >
+              Add
+            </Button>
+          </Form.Item>
+        </Form>
       </Col>
     </Row>
   );
